b47cd879d83561f5a1eeca05e5fa6584edf8c8aa
kunal <konnectwithkunal@gmail.com>
2025-08-14 17:55:35 +0530

    gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -7,8 +7,10 @@ import "swiper/css/pagination";
 
 import Lightbox from "yet-another-react-lightbox";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+
 import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
+
 
 import img1 from "@/assets/1.jpeg";
 import img2 from "@/assets/2.jpeg";
